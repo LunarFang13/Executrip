3e5e68022386e8a936f0a8119df5029a8cd87dfa
Kavin Manikandan <88193949+LunarFang13@users.noreply.github.com>
2023-03-31 23:37:33 +0530

    ads

---
+++ client/src/components/Login.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { loginFields } from "../constants/formFields";
+import FormAction from "./FormAction";
+import FormExtra from "./FormExtra";
+import Input from "./Input";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+
+const fields = loginFields;
+let fieldsState = {};
+fields.forEach((field) => (fieldsState[field.id] = ""));
+
+export default function Login() {
+  const [loginState, setLoginState] = useState(fieldsState);
+
+  const handleChange = (e) => {
+    setLoginState({ ...loginState, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+
+    console.log(loginState)
+    authenticateUser();
+    e.preventDefault();
+  };
+
+  //Handle Login API Integration here
+  const authenticateUser = () => {
+    fetch("https://temp-production-7bab.up.railway.app/api/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginState),
+    }).then((res) => {
+      if(res.status == 200) {
+        console.log(res.status)
+        var win = window.open("/city-select", "_self");
+      }
+    });
+  };
+
+  return (
+    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="-space-y-px">
+        {fields.map((field) => (
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={loginState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+            placeholder={field.placeholder}
+          />
+        ))}
+      </div>
+
+      <FormExtra />
+      <FormAction handleSubmit={handleSubmit} text="Login" />
+    </form>
+  );
+}
